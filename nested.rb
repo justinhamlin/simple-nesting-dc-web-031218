08023f65349f5edb442c0686bac2d94c63ea9879
Justin Hamlin <justin.hamlin@gmail.com>
2018-03-04 16:53:20 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].gsub
+     programmer_hash[:dennis_ritchie][:languages].gsub(/[], "")
 end
 
 def adding_matz
